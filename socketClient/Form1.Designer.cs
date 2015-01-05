71009b74c89927ade513d45ec4b9d279ece8c513
Omer <otariqmvc@gmail.com>
2015-01-05 12:40:45 +0500

    First Commit

---
+++ socketClient/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace socketClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_1 = new System.Windows.Forms.Button();
+            this.TextBox1 = new System.Windows.Forms.RichTextBox();
+            this.TextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(90, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // button_1
+            // 
+            this.button_1.Location = new System.Drawing.Point(177, 382);
+            this.button_1.Name = "button_1";
+            this.button_1.Size = new System.Drawing.Size(152, 23);
+            this.button_1.TabIndex = 3;
+            this.button_1.Text = "CLICK TO SEND DATA";
+            this.button_1.UseVisualStyleBackColor = true;
+            this.button_1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Location = new System.Drawing.Point(93, 120);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(306, 96);
+            this.TextBox1.TabIndex = 4;
+            this.TextBox1.Text = "";
+            // 
+            // TextBox2
+            // 
+            this.TextBox2.Location = new System.Drawing.Point(93, 240);
+            this.TextBox2.Name = "TextBox2";
+            this.TextBox2.Size = new System.Drawing.Size(306, 96);
+            this.TextBox2.TabIndex = 5;
+            this.TextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 476);
+            this.Controls.Add(this.TextBox2);
+            this.Controls.Add(this.TextBox1);
+            this.Controls.Add(this.button_1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void btn_Click_1(object sender, System.EventArgs e)
+        {
+            //throw new System.NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_1;
+        private System.Windows.Forms.RichTextBox TextBox1;
+        private System.Windows.Forms.RichTextBox TextBox2;
+    }
+}
+
---
+++ socketClient/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Text;
+using System.Net; 
+
+namespace socketClient
+{
+    public partial class Form1 : Form
+
+    {
+        System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+      
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            msg("Client Started");
+            clientSocket.Connect("127.0.0.1", 8888);
+            label1.Text = "Client Socket Program - Server Connected ...";
+
+                 
+        }
+
+            public void msg(string mesg)
+        {
+            TextBox1.Text = TextBox1.Text + Environment.NewLine + " >> " + mesg;
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            NetworkStream serverStream = clientSocket.GetStream();
+            byte[] outStream = System.Text.Encoding.ASCII.GetBytes(TextBox2.Text + "$");
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+
+            byte[] inStream = new byte[10025];
+            serverStream.Read(inStream, 0, (int)clientSocket.ReceiveBufferSize);
+            string returndata = System.Text.Encoding.ASCII.GetString(inStream);
+            msg(returndata);
+            TextBox2.Text = "";
+            TextBox2.Focus();
+        }
+
+      }
+}
